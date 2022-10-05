4b63c18fb7cf7cff785ebfd3ee21806604b1c2b7
ZiadAadim <ziad.aadim@student.ehb.be>
2022-10-05 12:19:19 +0200

    letters ziad work in progress

--- exercises/1-functions/1-1name.js
+++ exercises/1-functions/1-1name.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawName();
+
+function drawName() {
+    //Letter Z
+
+context.beginPath()
+context.moveTo(300, 300);
+context.lineTo(500, 300)
+context.lineTo(300, 500)
+context.stroke()
+context.moveTo()
+
+}
